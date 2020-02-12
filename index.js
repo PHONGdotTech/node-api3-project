cad719dbf7b07a21cf36a8f969c502a59217b99e
Phong Yang <phongyang88@hotmail.com>
2020-02-12 15:07:09 -0600

    updated and reinstalled dependencies. middleware within userRouter handled, not including custom middleware

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // code away!
 const express = require("express");
+const userRouter = require("./users/userRouter.js");
+
 const server = express();
 
 server.use(express.json());
 
-
+server.use("/api/users", userRouter)
 
 
 server.listen(5000, ()=>{
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,33 +1,84 @@
 const express = require('express');
+const Users = require("./userDb.js");
+const Posts = require("../posts/postDb.js");
 
 const router = express.Router();
 
 router.post('/', (req, res) => {
   // do your magic!
+  Users.insert(req.body)
+  .then(insertedUser =>{
+    res.status(201).json(req.body)
+  })
+  .catch(err => {
+    res.status(500).json({message: "There was an error adding a user to the server."})
+  })
 });
 
 router.post('/:id/posts', (req, res) => {
   // do your magic!
+  Posts.insert({...req.body, user_id: req.params.id})
+  .then(insertedPost =>{
+    res.status(201).json({...req.body, user_id: req.params.id})
+  })
+  .catch(err => {
+    res.status(500).json({message: "There was an error adding a post to the server."})
+  })
 });
 
 router.get('/', (req, res) => {
   // do your magic!
+  Users.get()
+  .then(users => {
+    res.status(200).json(users)
+  })
+  .catch(err => {
+    res.status(500).json({message: "There was an error getting users from the server."})
+  })
 });
 
 router.get('/:id', (req, res) => {
   // do your magic!
+  Users.getById(req.params.id)
+  .then(user => {
+    res.status(200).json(user)
+  })
+  .catch(err => {
+    res.status(500).json({message: "There was an error getting the user from the server."})
+  })
 });
 
 router.get('/:id/posts', (req, res) => {
   // do your magic!
+  Posts.getById(req.params.id)
+  .then(posts => {
+    res.status(200).json(posts)
+  })
+  .catch(err => {
+    res.status(500).json({message: "There was an error getting posts of that user from the server."})
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // do your magic!
+  Users.remove(req.params.id)
+  .then(removedCount => {
+    res.status(200).json({message: `Successfully removed ${removedCount} user.`})
+  })
+  .catch(err => {
+    res.status(500).json({message: "There was an error removing that user from the server."})
+  })
 });
 
 router.put('/:id', (req, res) => {
   // do your magic!
+  Users.update(req.params.id)
+  .then(updatedCount => {
+    res.status(200).json({message: `Successfully updated ${updatedCount} user.`})
+  })
+  .catch(err => {
+    res.status(500).json({message: "There was an error updating that user to the server."})
+  })
 });
 
 //custom middleware
