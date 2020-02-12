908a083c6c7892fa18137e4de58b35dba2c3e4b1
Phong Yang <phongyang88@hotmail.com>
2020-02-12 15:15:53 -0600

    updated all crud middleware on userRouter that include a user id to verify that the user exists before continuing

--- users/userRouter.js
+++ users/userRouter.js
@@ -17,12 +17,21 @@ router.post('/', (req, res) => {
 
 router.post('/:id/posts', (req, res) => {
   // do your magic!
-  Posts.insert({...req.body, user_id: req.params.id})
-  .then(insertedPost =>{
-    res.status(201).json({...req.body, user_id: req.params.id})
+  Users.getById(req.params.id)
+  .then(user => {
+    !user ? res.status(404).json({message: "No such user exists."}) : 
+    (
+      Posts.insert({...req.body, user_id: req.params.id})
+      .then(insertedPost =>{
+        res.status(201).json({...req.body, user_id: req.params.id})
+      })
+      .catch(err => {
+        res.status(500).json({message: "There was an error adding a post to the server."})
+      })
+    )
   })
   .catch(err => {
-    res.status(500).json({message: "There was an error adding a post to the server."})
+    res.status(500).json({message: "There was an error getting the user from the server."})
   })
 });
 
@@ -41,7 +50,7 @@ router.get('/:id', (req, res) => {
   // do your magic!
   Users.getById(req.params.id)
   .then(user => {
-    res.status(200).json(user)
+    !user ? res.status(404).json({message: "No such user exists."}) : res.status(200).json(user)
   })
   .catch(err => {
     res.status(500).json({message: "There was an error getting the user from the server."})
@@ -50,35 +59,68 @@ router.get('/:id', (req, res) => {
 
 router.get('/:id/posts', (req, res) => {
   // do your magic!
-  Posts.getById(req.params.id)
-  .then(posts => {
-    res.status(200).json(posts)
+  Users.getById(req.params.id)
+  .then(user => {
+    !user ? res.status(404).json({message: "No such user exists."}) :
+    (
+      Posts.getById(req.params.id)
+      .then(posts => {
+        res.status(200).json(posts)
+      })
+      .catch(err => {
+        res.status(500).json({message: "There was an error getting posts of that user from the server."})
+      })
+    )
   })
   .catch(err => {
-    res.status(500).json({message: "There was an error getting posts of that user from the server."})
+    res.status(500).json({message: "There was an error getting the user from the server."})
   })
+
+  
 });
 
 router.delete('/:id', (req, res) => {
   // do your magic!
-  Users.remove(req.params.id)
-  .then(removedCount => {
-    res.status(200).json({message: `Successfully removed ${removedCount} user.`})
+  Users.getById(req.params.id)
+  .then(user => {
+    !user ? res.status(404).json({message: "No such user exists."}) :
+    (
+      Users.remove(req.params.id)
+      .then(removedCount => {
+        res.status(200).json({message: `Successfully removed ${removedCount} user.`})
+      })
+      .catch(err => {
+        res.status(500).json({message: "There was an error removing that user from the server."})
+      })
+    )
   })
   .catch(err => {
-    res.status(500).json({message: "There was an error removing that user from the server."})
+    res.status(500).json({message: "There was an error getting the user from the server."})
   })
+
+  
 });
 
 router.put('/:id', (req, res) => {
   // do your magic!
-  Users.update(req.params.id)
-  .then(updatedCount => {
-    res.status(200).json({message: `Successfully updated ${updatedCount} user.`})
+  Users.getById(req.params.id)
+  .then(user => {
+    !user ? res.status(404).json({message: "No such user exists."}) :
+    (
+      Users.update(req.params.id)
+      .then(updatedCount => {
+        res.status(200).json({message: `Successfully updated ${updatedCount} user.`})
+      })
+      .catch(err => {
+        res.status(500).json({message: "There was an error updating that user to the server."})
+      })
+    )
   })
   .catch(err => {
-    res.status(500).json({message: "There was an error updating that user to the server."})
+    res.status(500).json({message: "There was an error getting the user from the server."})
   })
+
+  
 });
 
 //custom middleware
